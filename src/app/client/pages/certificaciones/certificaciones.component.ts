f93f82ece0af3d68b37c6f2abb7c802df24b7a87
Alan Bersia <alanbersia@gmail.com>
2024-03-11 12:23:07 -0300

    Cambios y agrego material

--- src/app/client/pages/certificaciones/certificaciones.component.ts
+++ src/app/client/pages/certificaciones/certificaciones.component.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-certificaciones',
   standalone: true,
   imports: [
     CommonModule,
+    MatDialogModule
     
   ],
   template: `
@@ -32,6 +36,9 @@ import { ActivatedRoute, Router } from '@angular/router';
         <div class="loader-dvl"></div>
       </div>
     }
+
+
+
     `,
   styleUrl: './certificaciones.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -40,7 +47,8 @@ export default class CertificacionesComponent {
   certificacion!: any;
   cargando!: boolean;
   constructor(private route: ActivatedRoute,
-    private router: Router) { }
+    private router: Router,
+    public dialog: MatDialog) { }
 
   ngOnInit() {
     this.cargando = true;
@@ -57,6 +65,48 @@ export default class CertificacionesComponent {
   };
 
   verImagen() {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      data: this.certificacion.img,
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed', result);
+    });
+  }
+}
+
+
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  template: `
+      <mat-dialog-content class="mat-typography">
+        <img class="imagen-completa" [src]="data" />
+      </mat-dialog-content>
+      <mat-dialog-actions align="end">
+        <button mat-button style="outline: none" color="primary" mat-dialog-close>Salir</button>
+      </mat-dialog-actions>
+  `,
+  standalone: true,
+  imports: [
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatButtonModule
+  ],
+})
+export class DialogOverviewExampleDialog implements OnInit{
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) {}
+
+
+  ngOnInit(): void {
+    console.log(this.data)
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app/app.component';
 import { InMemoryScrollingFeature, InMemoryScrollingOptions, provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app/app.routes';
 import { register as registerSwiperElements } from 'swiper/element/bundle';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 registerSwiperElements();
 
@@ -15,5 +16,5 @@ const scrollConfig: InMemoryScrollingOptions = {
 const inMemoryScrollingFeature: InMemoryScrollingFeature =
   withInMemoryScrolling(scrollConfig);
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes, inMemoryScrollingFeature)],
+  providers: [provideRouter(routes, inMemoryScrollingFeature), provideAnimationsAsync()],
 });

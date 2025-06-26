55e9987c04ba1a5a5c20c0b7f59ae536df1c9ac0
Alan Bersia <alanbersia@gmail.com>
2025-06-26 10:55:55 -0300

    Corrijo Navbar

---
+++ src/app/client/shared/footer/assets/data/casos-exito.ts
@@ -0,0 +1,83 @@
+export const casosExitoIOT = [
+    // {
+    //     img: 'assets/imgs/casos-exito/friar-logo.png',
+    //     problematica: ['Conocer el estado operativo de sus estaciones de bombeo.', 'Obtener la huella hídrica por kilo de animal.'],
+    //     solucion: ['Medición de Niveles y Caudales.'],
+    //     resultados: ['Instalación de sensores en bombas.', 'Volvió más eficiente el sistema.', 'Se logró a través de la plataforma medir la huella hídrica por kilo de animal.']
+    // },
+    // {
+    //     img: 'assets/imgs/casos-exito/geosistemas-logo.png',
+    //     problematica: ['Proporcionar herramientas de gestión de plagas y enfermedades con el objetivo de optimizar el uso de pesticidas e incrementar la calidad de la cosecha.'],
+    //     solucion: ['Integramos IoT Core de DVL al nuevo modelo de doble pulverizador con detectores de malezas, desarrollado por Geosistemas.'],
+    //     resultados: ['Las métricas y mediciones de pulverización pueden visualizarse en forma remota, garantizando la captura de datos sin pérdidas.', 'Los datos recolectados en sitio son enviados mediante tecnología de transmisión de datos de última generación, permitiendo ser utilizados mediante cualquier computadora o celular, gracias a la plataforma WEB responsive, y la APP DVL para Android.']
+    // },
+    {
+        img: 'assets/imgs/casos-exito/moreto.png',
+        problematica: ['Empresa familiar, ubicada en una zona estratégica del país, que desde hace más de 15 años brinda a otras empresas, servicios de almacenamiento, logística y distribución de productos. Dentro de los servicios que ofrece, se encuentra el almacenamiento en frío: 4000m2 de instalaciones con un riguroso control de temperatura y humedad, y personal capacitado para el perfecto control de las variables y la supervisión de la mercadería. La problemática presentada se relaciona con poder garantizar el mantenimiento de la cadena de frío.'],
+        solucion: ['Gestión de frío para agro'],
+        resultados: ['Realizar la gestión de frío en cámaras de almacenaje de semillas', 'Medición remota de temperatura y humedad en distintos puntos de la misma', 'Controlar aperturas y cierres de puertas', 'Supervisar el funcionamiento de los motores de refrigeración']
+    },
+    {
+        img: 'assets/imgs/casos-exito/pugnali.png',
+        problematica: ['Empresa especializada en diseño, construcción, montaje y provisión de equipamiento para Plantas de Ordeño, abastece insumos afines a la explotación lechera con planes anuales de venta programada; servicios de emergencias las 24 hs los 365 días del año y el mantenimiento programado en plantas de ordeño; análisis de aguas naturales para consumo humano, animal y riego en laboratorio propio; diseño y construcción de instalaciones con Sistema Hidrinox'],
+        solucion: ['Servicio de despacho de combustible y gestión de laboreo, así como también gestión de flotas de camiones a puerto'],
+        resultados: ['Control de consumo de combustible suministrado en planta, optimizando procesos, evaluando el correcto uso de la maquinaria, evaluando al personal, y colaborando con el control de faltantes. (despacho de combustible)', 'Permite tener información específica de la maquinaria agrícola o pesada, conociendo recorridos con y sin aplicación, ralenti, superficie trabajada. (gestión de laboreo)', 'Permite optimizar costos de logística, optimizar procesos y recorridos, controlar paradas indebidas, tiempos ociosos, circulación por zonas fuera de lo esperado, tener control de mantenimientos, y evaluar a los choferes. (gestión de flotas)']
+    },
+    {
+        img: 'assets/imgs/casos-exito/benevento.png',
+        problematica: ['Empresa comprometida a elaborar productos de calidad que pueda satisfacer necesidades de sus clientes. Para lograr este objetivo su equipo de especialistas mejora tanto en materia de maquinarias para la producción como en el control de calidad y procesos.'],
+        solucion: ['Gestión de Consumo Eléctrico'],
+        resultados: ['Medir de forma remota el consumo eléctrico de instalaciones de baja tensión.', 'Conocer el balanceo de fases, evitando sobrecargas de las mismas.', 'Conocer sobrecarga general, permitiendo tomar decisiones sobre el funcionamiento de la planta, antes de que el consumo se dispare y genere sobreprecios en la tarifa.', 'Conocer remotamente sobre cortes energéticos, que puedan afectar al funcionamiento de bombas, motores, o todo un proceso productivo.']
+    },
+    // {
+    //     img: 'assets/imgs/casos-exito/nestle.png',
+    //     problematica: ['El alto nivel de sinestrialidad que tenían en el proceso de recolección de la leche.'],
+    //     solucion: ['Gestión de flotas y Monitoreo Remoto de Tambos.'],
+    //     resultados: ['Evaluación de la conducción de los transportes y activa gestión sobre el control de velocidades y su discriminación por tipo de caminos.', 'Los accidentes con lesiones bajaron a cero. Tampoco se generaron daños a terceros.', 'Obtuvo 2º puesto para la categoría SAFE DRIVING del concurso “Global Nestlé Safety and Health Award”.']
+    // },
+    // {
+    //     img: 'assets/imgs/casos-exito/geosistemas-logo.png',
+    //     problematica: ['Desconocimiento de la utilización de la flota afectada a las diferentes áreas.', 'Falta de un sistema consolidado para vincular la Empresa con el Municipio.'],
+    //     solucion: ['Gestión de Flotas', 'Municipalidades afectadas: Villa María, Río Cuarto.', 'Río Tercero, Villa Carlos Paz y San Lorenzo.'],
+    //     resultados: ['Se logró versatilidad y la posibilidad de adaptarse a lo requerido según las circunstancias, gestionando los recursos disponibles, reduciendo los costos operativos y ayudando a los organismos gubernamentales a ser más eficientes. También mejoramos la evaluación de la utilización de las unidades afectadas a las diferentes áreas. Trabajando en parámetros como lo son las horas de conducción, velocidades y eficiencia en el ralenti.']
+    // },
+    // {
+    //     img: 'assets/imgs/casos-exito/venado-tuerto-gob.png',
+    //     problematica: ['Desconocimiento de la utilización de la flota afectada a las diferentes áreas.', 'Falta de un sistema consolidado para vincular la ciudadanía con el Municipio.', 'Portal desactualizado para la gestión de trámites y comunicación.'],
+    //     solucion: ['VLD Sistema de gestión de alerta ciudadana.', 'Gestión de Flotas', 'I+D Soluciones a medida.'],
+    //     resultados: ['Se logró versatilidad y la posibilidad de adaptarse a lo requerido según las circunstancias, gestionando los recursos disponibles, reduciendo los costos operativos y ayudando a los organismos gubernamentales a ser más eficientes. También mejoramos la evaluación de la utilización de las unidades afectadas a las diferentes áreas. Trabajando en parámetros como lo son las horas de conducción, velocidades y eficiencia en el ralenti.']
+    // }
+];
+
+export const casosExitoFlota = [
+    {
+        img: 'assets/imgs/casos-exito/friar-logo.png',
+        problematica: ['Conocer el estado operativo de sus estaciones de bombeo.', 'Obtener la huella hídrica por kilo de animal.'],
+        solucion: ['Medición de Niveles y Caudales.'],
+        resultados: ['Instalación de sensores en bombas.', 'Volvió más eficiente el sistema.', 'Se logró a través de la plataforma medir la huella hídrica por kilo de animal.']
+    },
+    // {
+    //     img: 'assets/imgs/casos-exito/geosistemas-logo.png',
+    //     problematica: ['Proporcionar herramientas de gestión de plagas y enfermedades con el objetivo de optimizar el uso de pesticidas e incrementar la calidad de la cosecha.'],
+    //     solucion: ['Integramos IoT Core de DVL al nuevo modelo de doble pulverizador con detectores de malezas, desarrollado por Geosistemas.'],
+    //     resultados: ['Las métricas y mediciones de pulverización pueden visualizarse en forma remota, garantizando la captura de datos sin pérdidas.', 'Los datos recolectados en sitio son enviados mediante tecnología de transmisión de datos de última generación, permitiendo ser utilizados mediante cualquier computadora o celular, gracias a la plataforma WEB responsive, y la APP DVL para Android.']
+    // },
+    {
+        img: 'assets/imgs/casos-exito/nestle.png',
+        problematica: ['El alto nivel de sinestrialidad que tenían en el proceso de recolección de la leche.'],
+        solucion: ['Gestión de flotas y Monitoreo Remoto de Tambos.'],
+        resultados: ['Evaluación de la conducción de los transportes y activa gestión sobre el control de velocidades y su discriminación por tipo de caminos.', 'Los accidentes con lesiones bajaron a cero. Tampoco se generaron daños a terceros.', 'Obtuvo 2º puesto para la categoría SAFE DRIVING del concurso “Global Nestlé Safety and Health Award”.']
+    },
+    // {
+    //     img: 'assets/imgs/casos-exito/geosistemas-logo.png',
+    //     problematica: ['Desconocimiento de la utilización de la flota afectada a las diferentes áreas.', 'Falta de un sistema consolidado para vincular la Empresa con el Municipio.'],
+    //     solucion: ['Gestión de Flotas', 'Municipalidades afectadas: Villa María, Río Cuarto.', 'Río Tercero, Villa Carlos Paz y San Lorenzo.'],
+    //     resultados: ['Se alcanzó una mayor versatilidad y capacidad de adaptación a las necesidades cambiantes, optimizando el uso de recursos disponibles, reduciendo costos operativos y contribuyendo a una mayor eficiencia en los organismos gubernamentales. Además, se mejoró la evaluación del desempeño de las unidades asignadas a diferentes áreas, trabajando con parámetros como horas de conducción, velocidades y eficiencia en ralentí.']
+    // },
+    {
+        img: 'assets/imgs/casos-exito/venado-tuerto-gob.png',
+        problematica: ['Desconocimiento de la utilización de la flota afectada a las diferentes áreas.', 'Falta de un sistema consolidado para vincular la ciudadanía con el Municipio.', 'Portal desactualizado para la gestión de trámites y comunicación.'],
+        solucion: ['VLD Sistema de gestión de alerta ciudadana.', 'Gestión de Flotas', 'I+D Soluciones a medida.'],
+        resultados: ['Se logró versatilidad y la posibilidad de adaptarse a lo requerido según las circunstancias, gestionando los recursos disponibles, reduciendo los costos operativos y ayudando a los organismos gubernamentales a ser más eficientes. También mejoramos la evaluación de la utilización de las unidades afectadas a las diferentes áreas. Trabajando en parámetros como lo son las horas de conducción, velocidades y eficiencia en el ralenti.']
+    }
+]
